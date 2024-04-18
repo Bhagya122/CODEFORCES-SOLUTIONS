731d6256bc9756178c419598c903af36e97dad4b
Bhagyashree <54356309+Bhagya122@users.noreply.github.com>
2024-04-18 21:02:46 +0530

    Update 118A-String Tasks.cpp

--- 118A-String Tasks.cpp
+++ 118A-String Tasks.cpp
@@ -7,7 +7,7 @@ int main()
   string input;
   std::cout<<"Please enter the word"<<std::endl;
   std::cin>>input;
-   string temp=tolower(input)
+   string temp=tolower(input);
   for(int i=0;i<=temp.size();i++)
 {
   if(temp[i]='a' || temp[i]='e' || temp[i]='i' ||temp[i]='o' || temp[i]='u' || temp[i]='y')
