2da8780de0ec8cee51fff375439c2b26f781ea51
Bhagyashree <54356309+Bhagya122@users.noreply.github.com>
2024-04-18 20:30:02 +0530

    Update 71A -Way too long word.cpp

--- 71A -Way too long word.cpp
+++ 71A -Way too long word.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  int n;
+  string input;
+  std::cout<<"Enter the word"<<std::endl;
+  std::cin>>input;
+  n=input.size();
+  if(n>10)
+ {
+  std::cout<<input[0]<<input.size()-2<<input[n-1]<<std::endl;
+ }
+ if(n<10)
+{ 
+ std::cout<<input<<std::endl;
+}
+  return 0;
+}
+
 
