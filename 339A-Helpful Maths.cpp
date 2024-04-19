fa298a9f253c220fbca8a08404be92987365ff2d
Bhagyashree <54356309+Bhagya122@users.noreply.github.com>
2024-04-19 14:43:54 +0530

    Update 339A-Helpful Maths.cpp

--- 339A-Helpful Maths.cpp
+++ 339A-Helpful Maths.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h> 
+using namespace std;
+int main()
+{
+    string s,temp;
+    std::cout<<"enter the addtion to be done"<<std::endl;
+    std::cin>>s;
+    for(int i=0;i<=s.size()-1;i++)
+    {
+        if(s[i]!='+')
+        {
+            temp.push_back(s[i]);
+        }
+    }
+    sort(temp.begin(),temp.end());
+    std::cout<<temp[0];
+    for(int i=1;i<=temp.size()-1;i++)
+    {
+        std::cout<<"+"<<temp[i];
+    }
+   return 0;
+    
+}
